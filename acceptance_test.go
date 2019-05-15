f856acb235f3770307f6a3c2c912324f05957ce0
Kiba Fox <kiba.fox@foxypossibilities.com>
2019-05-15 17:48:53 -0400

    Add some acceptance tests

--- acceptance_test.go
+++ acceptance_test.go
@@ -7,11 +7,56 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/KibaFox/tls-usr-sessions/pb"
 )
 
 var _ = Describe("Acceptance", func() {
+
+	It("Requires a CSR at login", func() {
+		conn, err := grpc.Dial(addr, grpc.WithInsecure())
+		Expect(err).ToNot(HaveOccurred(), "could not connect to: %s", addr)
+		defer conn.Close()
+
+		cli := pb.NewLoginClient(conn)
+		ctx, cancel := context.WithTimeout(
+			context.Background(), time.Second)
+		defer cancel()
+
+		By("Requesting login without a CSR")
+		_, err = cli.Login(ctx, &pb.LoginRequest{
+			Username: "demo",
+			Password: "password123",
+		})
+
+		Expect(err).To(HaveOccurred(), "server accepted the login request")
+		Expect(status.Code(err)).Should(Equal(codes.InvalidArgument))
+		s := status.Convert(err)
+		Expect(s.Message()).Should(Equal("a CSR is required"))
+	})
+
+	It("Should require username and password", func() {
+		conn, err := grpc.Dial(addr, grpc.WithInsecure())
+		Expect(err).ToNot(HaveOccurred(), "could not connect to: %s", addr)
+		defer conn.Close()
+
+		cli := pb.NewLoginClient(conn)
+		ctx, cancel := context.WithTimeout(
+			context.Background(), time.Second)
+		defer cancel()
+
+		By("Requesting login without a CSR")
+		_, err = cli.Login(ctx, &pb.LoginRequest{
+			Csr: "-",
+		})
+
+		Expect(err).To(HaveOccurred(), "server accepted the login request")
+		Expect(status.Code(err)).Should(Equal(codes.InvalidArgument))
+		s := status.Convert(err)
+		Expect(s.Message()).Should(Equal("username and password are required"))
+	})
 
 	It("Should allow login", func() {
 		conn, err := grpc.Dial(addr, grpc.WithInsecure())
@@ -26,6 +71,7 @@ var _ = Describe("Acceptance", func() {
 		resp, err := cli.Login(ctx, &pb.LoginRequest{
 			Username: "demo",
 			Password: "password123",
+			Csr:      "-",
 		})
 
 		Expect(err).ToNot(HaveOccurred(), "problem logging in")
--- server/login.go
+++ server/login.go
@@ -5,6 +5,8 @@ import (
 	"log"
 
 	"github.com/KibaFox/tls-usr-sessions/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // server is used to implement pb.LoginServer
@@ -18,6 +20,13 @@ func NewServer() *server {
 func (s *server) Login(
 	ctx context.Context, req *pb.LoginRequest,
 ) (resp *pb.LoginResponse, err error) {
+	if req.Csr == "" {
+		return nil, status.Error(codes.InvalidArgument, "a CSR is required")
+	}
+	if req.Username == "" || req.Password == "" {
+		return nil, status.Error(codes.InvalidArgument,
+			"username and password are required")
+	}
 	log.Printf("Received: %v", req)
 	return &pb.LoginResponse{Cert: "Got here!"}, nil
 }
